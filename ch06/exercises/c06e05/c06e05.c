86ac2b962ef64a81442d2faf072ecad85b92eb6a
Adam Carter <adam.clayton.carter@gmail.com>
2020-10-24 04:09:01 -0400

    Solve Exercise 5 from Chapter 6

---
+++ ch06/exercises/c06e05/c06e05.c
@@ -0,0 +1,29 @@
+/** (c) is not equivalent to (a) and (b) because the body
+ *  of the do-while statement will always be executed at
+ *  least once.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+
+    i = 10;
+    while (i < 10)
+        printf("%d ", i++);  /* branch is not entered */
+    printf("\n");
+
+    i = 10;
+    for (; i < 10;)
+        printf("%d ", i++);  /* branch is not entered */
+    printf("\n");
+
+    i = 10;
+    do {
+        printf("%d ", i++);  /* prints "10" */
+    } while (i < 10);
+    printf("\n");
+
+    return 0;
+}
