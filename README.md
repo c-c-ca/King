c9616adb8bc05d9ec8e0937838c47a333bdc1b47
Adam Carter <adam.clayton.carter@gmail.com>
2020-10-24 03:40:20 -0400

    Solve Exercise 1 from Chapter 6

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Solutions for C Programming: A Modern Approach, 2nd Edition by K. N. King
+# Solutions for *C Programming: A Modern Approach, 2nd Edition* by K. N. King
---
+++ ch06/exercises/c06e01/c06e01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+
+    i = 1;
+    while (i <= 128) {
+        printf("%d ", i);  // prints "1 2 4 8 16 32 64 128"
+        i *= 2;
+    }
+
+    printf("\n");
+
+    return 0;
+}
