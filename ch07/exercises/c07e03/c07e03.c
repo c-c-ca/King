9054979a14023b9b0b088d7657e0db49471f683b
Adam Carter <adam.clayton.carter@gmail.com>
2020-10-25 21:55:03 -0400

    Solve Exercise 3 from Chapter 7

---
+++ ch07/exercises/c07e03/c07e03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    short unsigned int su;     /* legal   */
+ /* short float sf; */         /* illegal */
+    long double ld;            /* legal   */
+    unsigned long ul;          /* legal   */
+
+    return 0;
+}
