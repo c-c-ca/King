c01570cdaa8d1cd340e39b56878a138297bb9b3d
Adam Carter <adam.clayton.carter@gmail.com>
2020-10-26 03:27:32 -0400

    Complete Project 4 from Chapter 7

---
+++ ch07/projects/c07p04/phone.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+    char ch;
+
+    printf("Enter phone number: ");
+    
+    while ((ch = getchar()) != '\n')
+        if ('A' <= ch && ch <= 'Z')
+            putchar('2' + (ch - 'A') / 3);
+        else
+            putchar(ch);
+
+    putchar('\n');
+
+    return 0;
+}
