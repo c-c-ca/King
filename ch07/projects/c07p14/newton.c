0a7d8a68e2fd148bd4070e43f1d90f067d1a2063
Adam Carter <adam.clayton.carter@gmail.com>
2020-10-26 22:16:39 -0400

    Complete Project 14 from Chapter 7

---
+++ ch07/projects/c07p14/newton.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    double x, y1, y2 = 1.0;
+
+    printf("Enter a positive number: ");
+    scanf("%lf", &x);
+
+    do {
+        y1 = y2;
+        y2 = (y1 + x / y1) / 2.0;
+    } while (fabs(y2 - y1) > 0.00001 * y1);
+
+    printf("Square root: %.5f\n", y2);
+
+    return 0;
+}
