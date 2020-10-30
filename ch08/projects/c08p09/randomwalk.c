33918b8230ae00293307bc652ef7462619d3dfae
Adam Carter <adam.clayton.carter@gmail.com>
2020-10-29 21:11:24 -0400

    Complete Project 9 from Chapter 8

---
+++ ch08/projects/c08p09/randomwalk.c
@@ -0,0 +1,55 @@
+/* Simulates a random walk */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N              10
+#define UP              0
+#define DOWN            1
+#define LEFT            2
+#define RIGHT           3
+#define NUM_DIRECTIONS  4
+
+int main(void)
+{
+    char matrix[N][N], step;
+    int x, y, direction;
+    bool valid_direction[NUM_DIRECTIONS];
+
+    srand((unsigned) time(NULL));
+
+    for (y = 0; y < N; y++)
+        for (x = 0; x < N; x++)
+            matrix[y][x] = '.';
+
+    x = y = 0;
+    for (step = 'A'; (matrix[y][x] = step) < 'Z'; step++) {
+        valid_direction[UP] = y > 0 && matrix[y-1][x] == '.';
+        valid_direction[DOWN] = y < N - 1 && matrix[y+1][x] == '.';
+        valid_direction[LEFT] = x > 0 && matrix[y][x-1] == '.';
+        valid_direction[RIGHT] = x < N - 1 && matrix[y][x+1] == '.';
+
+        if (!valid_direction[UP] && !valid_direction[DOWN] &&
+            !valid_direction[LEFT] && !valid_direction[RIGHT])
+            break;
+
+        while (!valid_direction[direction = rand() % NUM_DIRECTIONS])
+            ;
+        switch (direction) {
+            case UP:    y--; break;
+            case DOWN:  y++; break;
+            case LEFT:  x--; break;
+            case RIGHT: x++; break;
+        }
+    }
+
+    for (y = 0; y < N; y++) {
+        for (x = 0; x < N; x++)
+            printf("%c ", matrix[y][x]);
+        printf("\n");
+    }
+
+    return 0;
+}
