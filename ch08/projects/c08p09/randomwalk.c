ea4444c3419f95838f497df2fdc1eb387a8c399f
Adam Carter <adam.clayton.carter@gmail.com>
2020-10-30 04:21:41 -0400

    Update choice of variable names in Project 9 from Chapter 8

--- ch08/projects/c08p09/randomwalk.c
+++ ch08/projects/c08p09/randomwalk.c
@@ -16,22 +16,22 @@ int main(void)
 {
     char step, matrix[N][N] = {0};
     int x, y, direction;
-    bool valid_direction[NUM_DIRECTIONS];
+    bool legal_move[NUM_DIRECTIONS];
 
     srand((unsigned) time(NULL));
 
     x = y = 0;
     for (step = 'A'; (matrix[y][x] = step) < 'Z'; step++) {
-        valid_direction[UP] = (y > 0) && !matrix[y-1][x];
-        valid_direction[DOWN] = (y < N - 1) && !matrix[y+1][x];
-        valid_direction[LEFT] = (x > 0) && !matrix[y][x-1];
-        valid_direction[RIGHT] = (x < N - 1) && !matrix[y][x+1];
+        legal_move[UP] = (y > 0) && !matrix[y-1][x];
+        legal_move[DOWN] = (y < N - 1) && !matrix[y+1][x];
+        legal_move[LEFT] = (x > 0) && !matrix[y][x-1];
+        legal_move[RIGHT] = (x < N - 1) && !matrix[y][x+1];
 
-        if (!valid_direction[UP] && !valid_direction[DOWN] &&
-            !valid_direction[LEFT] && !valid_direction[RIGHT])
+        if (!legal_move[UP] && !legal_move[DOWN] &&
+            !legal_move[LEFT] && !legal_move[RIGHT])
             break;
 
-        while (!valid_direction[direction = rand() % NUM_DIRECTIONS])
+        while (!legal_move[direction = rand() % NUM_DIRECTIONS])
             ;
             
         switch (direction) {
