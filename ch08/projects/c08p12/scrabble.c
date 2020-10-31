48b9bed106ff7f5e99715a3fa11c57cd04eb43bd
Adam Carter <adam.clayton.carter@gmail.com>
2020-10-31 02:30:49 -0400

    Complete Project 12 from Chapter 8

---
+++ ch08/projects/c08p12/scrabble.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+    const int letter_values[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 6, 5, 1, 3, 
+                                 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 6, 4, 10};
+    int sum = 0;
+    char ch;
+
+    printf("Enter a word: ");
+
+    while ((ch = getchar()) != '\n')
+        sum += letter_values[tolower(ch) - 'a'];
+
+    printf("Scrabble value: %d\n", sum);
+
+    return 0;
+}
