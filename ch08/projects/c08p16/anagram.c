b74a4d50e3c0ceba3351b00c3c56037b41c8bc19
Adam Carter <adam.clayton.carter@gmail.com>
2020-10-31 21:05:04 -0400

    Complete Project 16 from Chapter 8

---
+++ ch08/projects/c08p16/anagram.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define NUM_LETTERS 26
+
+int main(void)
+{
+    char ch;
+    int i, letter_counts[NUM_LETTERS] = {0};
+
+    printf("Enter first word: ");
+    while (isalpha(ch = getchar()))
+        letter_counts[toupper(ch) - 'A']++;
+
+    printf("Enter second word: ");
+    while (isalpha(ch = getchar()))
+        letter_counts[toupper(ch) - 'A']--;
+
+    for (i = 0; i < NUM_LETTERS; i++)
+        if (letter_counts[i] != 0)
+            break;
+
+    if (i == NUM_LETTERS)
+        printf("The words are anagrams.\n");
+    else
+        printf("The words are not anagrams.\n");
+
+    return 0;
+}
