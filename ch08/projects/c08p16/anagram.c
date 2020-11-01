8ca63c7296d1ec127b55cbbb53714a9c39b8d32d
Adam Carter <adam.clayton.carter@gmail.com>
2020-10-31 21:07:01 -0400

    Modify Program 16 from Chapter 8 to more closely reflect the suggestions made in the problem statement

--- ch08/projects/c08p16/anagram.c
+++ ch08/projects/c08p16/anagram.c
@@ -10,11 +10,11 @@ int main(void)
 
     printf("Enter first word: ");
     while (isalpha(ch = getchar()))
-        letter_counts[toupper(ch) - 'A']++;
+        letter_counts[tolower(ch) - 'a']++;
 
     printf("Enter second word: ");
     while (isalpha(ch = getchar()))
-        letter_counts[toupper(ch) - 'A']--;
+        letter_counts[tolower(ch) - 'a']--;
 
     for (i = 0; i < NUM_LETTERS; i++)
         if (letter_counts[i] != 0)
