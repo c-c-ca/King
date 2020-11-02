7d2bc5100bd9a09055008aca494b7f389516d8ef
Adam Carter <adam.clayton.carter@gmail.com>
2020-11-02 02:25:53 -0500

    Solve Exercise 3 from Chapter 9

---
+++ ch09/exercises/c09e03/gcd.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int gcd(int m, int n);
+int main(void)
+{
+    int x, y;
+
+    printf("Enter two integers: ");
+    scanf("%d%d", &x, &y);
+    printf("Greatest common divisor: %d", x, y, gcd(x, y));
+
+    return 0;
+}
+
+int gcd(int m, int n)
+{
+    int remainder;
+
+    while (n != 0) {
+        remainder = m % n;
+        m = n;
+        n = remainder;
+    }
+
+    return m;
+}
