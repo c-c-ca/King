5f41d3923ddb5f04e56e53b324a790bd7e4f5b93
Adam Carter <adam.clayton.carter@gmail.com>
2020-11-02 21:02:06 -0500

    Modify Project 9 from Chapter 8 to incorporate instructions given in Project 3 from Chapter 9

--- ch08/projects/c08p09/randomwalk.c
+++ ch08/projects/c08p09/randomwalk.c
@@ -6,25 +6,29 @@
 #include <time.h>
 
 #define N              10
-#define UP              0
-#define DOWN            1
-#define LEFT            2
-#define RIGHT           3
-#define NUM_DIRECTIONS  4
+#define UP             0
+#define DOWN           1
+#define LEFT           2
+#define RIGHT          3
+#define NUM_DIRECTIONS 4
 
 int main(void)
 {
     bool legal_move[NUM_DIRECTIONS];
-    char matrix[N][N] = {0};
+    char walk[N][N];
     int x, y, step, direction;
 
     srand((unsigned) time(NULL));
 
-    for (x = y = step = 0; (matrix[y][x] = 'A' + step) < 'Z'; step++) {
-        legal_move[UP] = (y > 0) && !matrix[y-1][x];
-        legal_move[DOWN] = (y < N - 1) && !matrix[y+1][x];
-        legal_move[LEFT] = (x > 0) && !matrix[y][x-1];
-        legal_move[RIGHT] = (x < N - 1) && !matrix[y][x+1];
+    for (y = 0; y < N; y++)
+        for (x = 0; x < N; x++)
+            walk[y][x] = '.';
+
+    for (x = y = step = 0; (walk[y][x] = 'A' + step) < 'Z'; step++) {
+        legal_move[UP] = (y > 0) && walk[y-1][x] == '.';
+        legal_move[DOWN] = (y < N - 1) && walk[y+1][x] == '.';
+        legal_move[LEFT] = (x > 0) && walk[y][x-1] == '.';
+        legal_move[RIGHT] = (x < N - 1) && walk[y][x+1] == '.';
 
         if (!legal_move[UP] && !legal_move[DOWN] &&
             !legal_move[LEFT] && !legal_move[RIGHT])
@@ -43,7 +47,7 @@ int main(void)
 
     for (y = 0; y < N; y++) {
         for (x = 0; x < N; x++)
-            printf("%c ", matrix[y][x] ? matrix[y][x] : '.');
+            printf("%c ", walk[y][x]);
         printf("\n");
     }
 
