2b41b81442fd718bf7d38db1d00e290a5d71aeba
Adam Carter <adam.clayton.carter@gmail.com>
2020-11-16 15:03:30 -0500

    Solve Exercise 3 from Chapter 12

---
+++ ch12/exercises/c12e03/c12e03.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define N 10
+
+int main(void)
+{
+    int a[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int *p = &a[0], *q = &a[N-1], temp;
+
+    while (p < q) {
+        temp = *p;
+        *p++ = *q;
+        *q-- = temp;
+    }
+
+    for (p = a; p < a + N;)
+        printf("%d ", *p++);   /* prints "10 9 8 7 6 5 4 3 2 1" */
+    printf("\n");
+}
