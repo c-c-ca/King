d503a861dc27acd4438ab8a27aa6a06acd91f2ff
Adam Carter <adam.clayton.carter@gmail.com>
2020-11-17 16:00:12 -0500

    Ignore newline character

--- ch12/projects/c12p01/reverse.c
+++ ch12/projects/c12p01/reverse.c
@@ -7,8 +7,8 @@ int main(void)
     char msg[MAX_LEN], *p = msg;
 
     printf("Enter a message: ");
-    while (p - msg < MAX_LEN && (*p++ = getchar()) != '\n') 
-        ;
+    while (p - msg < MAX_LEN && (*p = getchar()) != '\n') 
+        p++;
 
     printf("Reversal is: ");
     while (p-- > msg)
