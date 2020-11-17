2f96bb6a1335497388ce6e3f9559e48a247acf32
Adam Carter <adam.clayton.carter@gmail.com>
2020-11-17 15:30:10 -0500

    Complete Project 1 from Chapter 12

---
+++ ch12/projects/c12p01/reverse.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define MAX_LEN 80    /* maximum length of message */
+
+int main(void)
+{
+    char msg[MAX_LEN], *p = msg;
+
+    printf("Enter a message: ");
+    while (p - msg < MAX_LEN && (*p++ = getchar()) != '\n') 
+        ;
+
+    printf("Reversal is: ");
+    while (p-- > msg)
+        putchar(*p);
+    printf("\n");
+
+    return 0;
+}
