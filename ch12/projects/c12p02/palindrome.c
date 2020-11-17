6db70d4ac406ff91482538d1908c5a529636eacc
Adam Carter <adam.clayton.carter@gmail.com>
2020-11-17 16:20:28 -0500

    Change MAX_LEN to MSG_LEN

--- ch12/projects/c12p02/palindrome.c
+++ ch12/projects/c12p02/palindrome.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <ctype.h>
 
-#define MAX_LEN 80    /* maximum length of message */
+#define MSG_LEN 80    /* maximum length of message */
 
 int main(void)
 {
-    char msg[MAX_LEN], *p, *q;
+    char msg[MSG_LEN], *p, *q;
 
     p = q = msg;
 
     printf("Enter a message: ");
-    while (p - msg < MAX_LEN && (*p = getchar()) != '\n') 
+    while (p - msg < MSG_LEN && (*p = getchar()) != '\n') 
         if (isalnum(*p))
             p++;
 
