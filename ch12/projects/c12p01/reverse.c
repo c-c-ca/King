c982a78ab7d7770198b6261672e5c777049fda44
Adam Carter <adam.clayton.carter@gmail.com>
2020-11-17 16:21:22 -0500

    Change MAX_LEN to MSG_LEN

--- ch12/projects/c12p01/reverse.c
+++ ch12/projects/c12p01/reverse.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-#define MAX_LEN 80    /* maximum length of message */
+#define MSG_LEN 80    /* maximum length of message */
 
 int main(void)
 {
-    char msg[MAX_LEN], *p = msg;
+    char msg[MSG_LEN], *p = msg;
 
     printf("Enter a message: ");
-    while (p - msg < MAX_LEN && (*p = getchar()) != '\n') 
+    while (p - msg < MSG_LEN && (*p = getchar()) != '\n') 
         p++;
 
     printf("Reversal is: ");
