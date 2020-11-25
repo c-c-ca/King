21e40f4e3e5185edf04b3327653d362f9164b3e8
Adam Carter <adam.clayton.carter@gmail.com>
2020-11-24 19:24:54 -0500

    Solve Exercise 17 from Chapter 13

---
+++ ch13/exercises/c13e17/testextension.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+bool test_extension(const char *file_name,
+                    const char *extension);
+
+int main(void)
+{
+    if (test_extension("memo.txt", "TXT"))
+        printf("Extension found in file name\n");
+    else
+        printf("Extension not found in file name\n");
+
+    return 0;
+}
+
+bool test_extension(const char *file_name,
+                    const char *extension)
+{
+    const char *p = file_name, *q = extension;
+
+    while (*p++ != '.')
+        ;
+
+    while (*p && *q)
+        if (toupper(*p++) != toupper(*q++))
+            return false;
+
+    return *p == *q;
+}
