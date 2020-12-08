b9125704cb757b33d91900fd8bcbb74cc0c5357f
Adam Carter <adam.clayton.carter@gmail.com>
2020-12-08 16:13:55 -0500

    Complete Project 7 from Chapter 13

---
+++ ch13/projects/c13p07/twodigit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    const char *ones[] = {"", "one", "two", "three", "four", "five", 
+                          "six", "seven", "eight", "nine"};
+    const char *tens[] = {"", "", "twenty", "thirty", "forty", "fifty",
+                          "sixty", "seventy", "eighty", "ninety"};
+    const char *teens[] = {"ten", "elvent", "twelve", "thirteen", 
+                           "fourteen", "fifteen", "sixteen", "seventeen",
+                           "eighteen", "nineteen"};
+
+    printf("Enter a two digit number: ");
+    scanf("%2d", &n);
+
+    printf("You entered the number: ");
+    if (n / 10 == 1)
+        printf("%s", teens[n % 10]); 
+    else
+        printf("%s%s%s",
+                tens[n / 10],
+                n / 10 != 0 && n % 10 != 0 ? "-" : "",
+                ones[n % 10]);
+    printf(".\n");
+
+    return 0;
+}
