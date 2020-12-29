aea0f9bfcfe71af4b2fc3a60c886cd6e803686db
Adam Carter <adam.clayton.carter@gmail.com>
2020-12-29 15:51:54 -0500

    Add documentation to readline.h

--- ch15/projects/c15p04/readline.h
+++ ch15/projects/c15p04/readline.h
@@ -1,1 +1,11 @@
+#ifndef READLINE_H
+#define READLINE_H
+
+/*************************************************************
+ * read_line: Stores the first n characters of the current   *
+ *            current line in str.  Discards all remaining   *
+ *            characters until the next line is encountered. *
+ *************************************************************/
 int read_line(char str[], int n);
+
+#endif
