ff583c20a81886ad191899973a9dce18c5edc1c2
Adam Carter <adam.clayton.carter@gmail.com>
2020-12-29 15:41:03 -0500

    Add documentation to quicksort.h

--- ch15/projects/c15p03/quicksort.h
+++ ch15/projects/c15p03/quicksort.h
@@ -1,2 +1,16 @@
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+/***********************************************************
+ * quicksort: Sorts all numbers in a from low to hight.    *
+ ***********************************************************/
 void quicksort(int a[], int low, int high);
+
+/***********************************************************
+ * split: Places all numbers in a from low to high so that *
+ *        all elements greater than the pivot element      *
+ *        occur after it.  Returns the pivot element.      *
+ ***********************************************************/
 int split(int a[], int low, int high);
+
+#endif
