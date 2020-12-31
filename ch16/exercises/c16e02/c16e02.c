a2143d6574a87330e0a924144cdf2392547da3cd
Adam Carter <adam.clayton.carter@gmail.com>
2020-12-31 15:21:01 -0500

    Solve Exercise 2 from Chapter 16

---
+++ ch16/exercises/c16e02/c16e02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define PRINT(x) printf("Value of " #x ": %g\n", x)
+
+int main(void)
+{
+    struct {
+        double imaginary;
+        double real;
+    } c1 = { 0.0, 1.0 }, 
+      c2 = { 1.0, 0.0 }, 
+      c3;
+
+    c3.imaginary = c1.imaginary + c2.imaginary;
+    c3.real = c1.real + c2.real;
+
+    PRINT(c3.imaginary);
+    PRINT(c3.real);
+
+    return 0;
+}
