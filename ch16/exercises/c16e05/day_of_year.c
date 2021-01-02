272dd14fee203a0d361b5d82e6e900d386f4d013
Adam Carter <adam.clayton.carter@gmail.com>
2021-01-02 16:20:21 -0500

    Modify solution for Exercise 5 from Chapter 16

--- ch16/exercises/c16e05/day_of_year.c
+++ ch16/exercises/c16e05/day_of_year.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define PRINT_DATE(x) printf("%d-%d-%d", x.year, x.month, x.day)
+
 struct date {
     int month;
     int day;
@@ -7,19 +9,39 @@ struct date {
 };
 
 int day_of_year(struct date d);
+int compare_dates(struct date d1, struct date d2);
 
 int main(void)
 {
-    struct date d;
+    struct date d1, d2;
 
     printf("Enter month: ");
-    scanf("%d", &d.month);
+    scanf("%d", &d1.month);
     printf("Enter day: ");
-    scanf("%d", &d.day);
+    scanf("%d", &d1.day);
     printf("Enter year: ");
-    scanf("%d", &d.year);
+    scanf("%d", &d1.year);
 
-    printf("Day of year: %d\n", day_of_year(d));
+    printf("Day of year: %d\n", day_of_year(d1));
+
+    printf("Enter month: ");
+    scanf("%d", &d2.month);
+    printf("Enter day: ");
+    scanf("%d", &d2.day);
+    printf("Enter year: ");
+    scanf("%d", &d2.year);
+
+    printf("Day of year: %d\n", day_of_year(d2));
+
+    if (compare_dates(d1, d2) < 0) {
+        PRINT_DATE(d1);
+        printf(" is earlier than ");
+        PRINT_DATE(d2);
+    } else {
+        PRINT_DATE(d1);
+        printf(" is not earlier than ");
+        PRINT_DATE(d2);
+    }
 
     return 0;
 }
@@ -37,4 +59,18 @@ int day_of_year(struct date d)
         day_count++;
 
     return day_count + d.day;
+}
+
+int compare_dates(struct date d1, struct date d2)
+{
+    if (d1.year != d2.year)
+        return d1.year < d2.year ? -1 : 1;
+
+    if (d1.month != d2.month)
+        return d1.month < d2.month ? -1 : 1;
+
+    if (d1.day != d2.day)
+        return d1.day < d2.day ? -1 : 1;
+
+    return 0;
 }
