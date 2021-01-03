84d508524edea5b5b320af8f75788faa7a4c2af1
Adam Carter <adam.clayton.carter@gmail.com>
2021-01-02 21:31:31 -0500

    Solve Exercise 10 from Chapter 16

---
+++ ch16/exercises/c16e10/graphics.c
@@ -0,0 +1,89 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+#define MIN(x,y)  ((x) < (y) ? (x) : (y))
+#define MAX(x,y)  ((x) > (y) ? (x) : (y))
+#define MIN_X(r)  (MIN((r).upper_left.x, (r).lower_right.x))
+#define MAX_X(r)  (MAX((r).upper_left.x, (r).lower_right.x))
+#define MIN_Y(r)  (MIN((r).upper_left.y, (r).lower_right.y))
+#define MAX_Y(r)  (MAX((r).upper_left.y, (r).lower_right.y))
+#define WIDTH(r)  (MAX_X(r) - MIN_X(r))
+#define LENGTH(r) (MAX_Y(r) - MIN_Y(r))
+
+#define PRINT_NL() printf("\n")
+
+#define PRINT_POINT(p)                \
+    printf("point: x = %d, y = %d; ", \
+            (p).x, (p).y)
+
+#define PRINT_RECTANGLE(r)            \
+    do {                              \
+        printf("rectangle: ");        \
+        printf("upper_left = ");      \
+        PRINT_POINT((r).upper_left);  \
+        printf("lower_right = ");     \
+        PRINT_POINT((r).lower_right); \
+    } while (false)
+
+struct point { int x, y; };
+struct rectangle { struct point upper_left, lower_right; };
+
+int rectangle_area(struct rectangle r);
+struct point getCenter(struct rectangle r);
+struct point move_point(struct point p, int x, int y);
+struct rectangle move_rectangle(struct rectangle r, int x, int y);
+bool contains_point(struct rectangle r, struct point p);
+
+int main(void)
+{
+    struct point p1 = {0, 0}, p2 = {10, 5}, p3;
+    struct rectangle r = {p1, p2};
+
+    PRINT_RECTANGLE(r);
+    PRINT_NL();
+    PRINT_POINT(getCenter(r));
+    PRINT_NL();
+
+    r = move_rectangle(r, 3, 5);
+    PRINT_RECTANGLE(r);
+    PRINT_NL();
+
+    if (contains_point(r, getCenter(r)))
+        printf("Point is inside of rectangle.\n");
+    else
+        printf("Point is outside of rectangle.\n");
+
+    return 0;
+}
+
+int rectangle_area(struct rectangle r)
+{
+    return WIDTH(r) * LENGTH(r);
+}
+
+struct point getCenter(struct rectangle r)
+{
+    return (struct point) {
+        (r.upper_left.x + r.lower_right.x) / 2,
+        (r.upper_left.y + r.lower_right.y) / 2
+    };
+}
+
+struct point move_point(struct point p, int x, int y)
+{
+    return (struct point) {p.x + x, p.y + y};
+}
+
+struct rectangle move_rectangle(struct rectangle r, int x, int y)
+{
+    return (struct rectangle) {
+        move_point(r.upper_left, x, y),
+        move_point(r.lower_right, x, y)
+    };
+}
+
+bool contains_point(struct rectangle r, struct point p)
+{
+    return MIN_X(r) <= p.x < MAX_X(r) &&
+           MIN_Y(r) <= p.y < MAX_Y(r);
+}
