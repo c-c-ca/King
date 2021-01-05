f858f4324f4b0f1e5904fa1c68836df3e2a66d3e
Adam Carter <adam.clayton.carter@gmail.com>
2021-01-04 19:13:11 -0500

    Solve Exercise 15 from Chapter 16

---
+++ ch16/exercises/c16e15/weekdays.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#ifdef A
+enum week_days {MON, TUE, WED, THU, FRI, SAT, SUN};
+#endif
+
+#ifdef B
+typedef enum {MON, TUE, WED, THU, FRI, SAT, SUN} Week_days;
+#endif
+
+int main(void)
+{
+#ifdef A
+    enum week_days day = MON;
+#endif
+
+#ifdef B
+    Week_days day = TUE;
+#endif
+
+    printf("%d\n", day);
+
+    return 0;
+}
