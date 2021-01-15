9f4d766f14dc1bd9caa102173dd6cbfeff342c19
Adam Carter <adam.clayton.carter@gmail.com>
2021-01-14 19:18:51 -0500

    Solve Exercise 13 from Chapter 17

---
+++ ch17/exercises/c17e13/linkedlist.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int value;           /* data stored in the node  */
+    struct node *next;   /* pointer to the next node */
+};
+
+struct node *first = NULL;
+
+void add_to_list(struct node **list, int n);
+struct node *read_numbers(void);
+struct node *search_list(struct node *list, int n);
+struct node *delete_from_list(struct node *list, int n);
+struct node *insert_into_ordered_list(struct node *list,
+                                      struct node *new_node);
+
+int main(void)
+{
+    struct node *p;
+
+    read_numbers();
+    for (p = first; p != NULL; p = p->next)
+        printf("%d ", *p);
+    printf("\n");
+
+    return 0;
+}
+
+void add_to_list(struct node **list, int n)
+{
+    struct node *new_node;
+
+    new_node = malloc(sizeof(struct node));
+    if (new_node == NULL) {
+        printf("Error: malloc failed in add_to_list\n");
+        exit(EXIT_FAILURE);
+    }
+    new_node->value = n;
+    new_node->next = *list;
+    *list = insert_into_ordered_list(*list, new_node);
+}
+
+struct node *read_numbers(void)
+{
+    int n;
+
+    printf("Enter a series of integers (0 to terminate): ");
+    for (;;) {
+        scanf("%d", &n);
+        if (n == 0)
+            return first;
+        add_to_list(&first, n);
+    }
+}
+
+struct node *search_list(struct node *list, int n)
+{
+    while (list != NULL && list->value != n)
+        list = list->next;
+    return list;
+}
+
+struct node *delete_from_list(struct node *list, int n)
+{
+    struct node *cur, *prev;
+
+    for (cur = list, prev = NULL;
+         cur != NULL && cur->value != n;
+         prev = cur, cur = cur->next)
+        ;
+    if (cur == NULL)
+        return list;              /* n was not found */
+    if (prev == NULL)
+        list = list->next;        /* n is in the first node */
+    else
+        prev->next = cur->next;   /* n is in some other node */
+    free(cur);
+    return list;
+}
+
+/*
+struct node *insert_into_ordered_list(struct node *list,
+                                      struct node *new_node)
+{
+    struct node *cur = list, *prev = NULL;
+    while (cur->value <= new_node->value) {
+        prev = cur;
+        cur = cur->next;
+    }
+    prev->next = new_node;
+    new_node->next = cur;
+    return list;
+}
+*/
+
+struct node *insert_into_ordered_list(struct node *list,
+                                      struct node *new_node)
+{
+    struct node *cur = list, *prev = NULL;
+    while (cur != NULL && cur->value <= new_node->value) {
+        prev = cur;
+        cur = cur->next;
+    }
+    new_node->next = cur;
+    if (prev == NULL)
+        return new_node;
+    prev->next = new_node;
+    return list;
+}
