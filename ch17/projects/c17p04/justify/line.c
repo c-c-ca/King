9ebb075e2f3270b7b04e0698d31827bc5a10e8ee
Adam Carter <adam.clayton.carter@gmail.com>
2021-01-17 20:56:52 -0500

    Complete Project 4 from Chapter 17

---
+++ ch17/projects/c17p04/justify/line.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "line.h"
+
+#define MAX_LINE_LEN 60
+
+struct word {
+    struct word *next;
+    char chars[];
+};
+
+struct word *line = NULL;
+int line_len = 0;
+int num_words = 0;
+
+void clear_line(void)
+{
+    struct word *temp;
+
+    while (line != NULL) {
+        temp = line->next;
+        free(line);
+        line = temp;
+    }
+
+    line_len = 0;
+    num_words = 0;
+}
+
+void add_word(const char *word)
+{
+    struct word **p = &line;
+    int word_len = strlen(word);
+
+    if (*p != NULL) {
+        do {
+            p = &(*p)->next;
+        } while (*p != NULL);
+        line_len++;
+    }
+
+    if ((*p = malloc(sizeof(struct word) + word_len + 1)) == NULL) {
+        printf("Error: malloc failed in add_word\n");
+        exit(EXIT_FAILURE);
+    }
+
+    (*p)->next = NULL;
+    strcpy((*p)->chars, word);
+    line_len += word_len;
+    num_words++;
+}
+
+int space_remaining(void)
+{
+    return MAX_LINE_LEN - line_len;
+}
+
+void write_line(void)
+{
+    struct word *p;
+    int i, extra_spaces, spaces_to_insert;
+
+    extra_spaces = MAX_LINE_LEN - line_len;
+    for (p = line; p != NULL; p = p->next) {
+        printf(p->chars);
+        if (p->next != NULL) {
+            spaces_to_insert = extra_spaces / (num_words - 1);
+            for (i = 1; i <= spaces_to_insert + 1; i++)
+                putchar(' ');
+            extra_spaces -= spaces_to_insert;
+            num_words--;
+        }
+    }
+    putchar('\n');
+}
+
+void flush_line(void)
+{
+    struct word *p;
+
+    for (p = line; p != NULL; p = p->next)
+        printf("%s%c", p->chars, p->next != NULL ? ' ' : '\n');
+}
