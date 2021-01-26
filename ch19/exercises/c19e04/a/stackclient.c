f7f396210b77c21b60f48109eb94e4fc88169e69
Adam Carter <adam.clayton.carter@gmail.com>
2021-01-26 18:20:53 -0500

    Solve Exercise 4 from Chapter 19

---
+++ ch19/exercises/c19e04/a/stackclient.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "stack.h"
+
+int main(void)
+{
+    Stack s1, s2;
+
+    make_empty(&s1);
+    make_empty(&s2);
+    push(&s1, 1);
+    push(&s2, 2);
+    if (!is_empty(&s1))
+        printf("%d\n", pop(&s1));   /* prints "1" */
+
+    return 0;
+}
