bdb56160fd8f9a4d7b604f9afc0137a4c00cbae4
Adam Carter <adam.clayton.carter@gmail.com>
2021-01-27 18:43:10 -0500

    Complete Project 1 from Chapter 19

---
+++ ch19/projects/c19p01/nested.c
@@ -0,0 +1,27 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include "stackADT.h"
+
+int main(void)
+{
+    char ch;
+    bool is_nested_properly = true;
+    Stack s = create();
+
+    printf("Enter parentheses and/or braces: ");
+    while ((ch = getchar()) != '\n')
+        if (ch == '(' || ch == '{')
+            push(s, ch);
+        else if ((ch == ')' && pop(s) != '(') ||
+                 (ch == '}' && pop(s) != '{')) {
+            is_nested_properly = false;
+            break;
+        }
+    
+    if (is_nested_properly && is_empty(s))
+        printf("Parentheses/braces are nested properly\n");
+    else
+        printf("Parentheses/braces are not nested properly\n");
+        
+    return 0;
+}
