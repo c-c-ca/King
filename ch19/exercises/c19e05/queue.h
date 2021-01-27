2f47043b0d3169e8803a243c542999beb649cde0
Adam Carter <adam.clayton.carter@gmail.com>
2021-01-27 17:11:45 -0500

    Solve Exercise 5 from Chapter 19

---
+++ ch19/exercises/c19e05/queue.h
@@ -0,0 +1,21 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdbool.h>   /* C99 only */
+
+#define QUEUE_SIZE 100
+
+typedef struct {
+    int contents[QUEUE_SIZE];
+    int first;
+    int last;
+    int size;
+} Queue;
+
+void insert_item(Queue *q, int i);
+int remove_item(Queue *q);
+int first_item(const Queue *q);
+int last_item(const Queue *q);
+bool is_empty(const Queue *q);
+
+#endif
