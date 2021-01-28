bef7cbb5cf2afe9108b788a4f9e85b9ea90f266d
Adam Carter <adam.clayton.carter@gmail.com>
2021-01-28 17:15:49 -0500

    Modify Project 6 from Chapter 19

--- ch19/projects/c19p06/queueADT2.c
+++ ch19/projects/c19p06/queueADT2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "queueADT.h"
+#include "queueADT2.h"
 
 struct queue_type {
     Item *contents;
--- ch19/projects/c19p06/queueclient.c
+++ ch19/projects/c19p06/queueclient.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "queueADT.h"
+#include "queueADT2.h"
 
 int main(void)
 {
