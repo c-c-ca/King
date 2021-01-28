8beca669d6b60c0d76a53c8fc6999a0dba2b3cfb
Adam Carter <adam.clayton.carter@gmail.com>
2021-01-28 16:40:15 -0500

    Complete Project 4 from Chapter 19

---
+++ ch19/projects/c19p04/stackclient.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "stackADT.h"
+
+int main(void)
+{
+    Stack s1, s2;
+    char *str;
+
+    s1 = create();
+    s2 = create();
+
+    push(s1, "foo");
+    push(s1, "bar");
+
+    str = pop(s1);
+    printf("Popped %s from s1\n", str);
+    push(s2, str);
+    str = pop(s1);
+    printf("Popped %s from s1\n", str);
+    push(s2, str);
+
+    destroy(s1);
+
+    while (!is_empty(s2))
+        printf("Popped %s from s2\n", pop(s2));
+
+    push(s2, "baz");
+    make_empty(s2);
+    if (is_empty(s2))
+        printf("s2 is empty\n");
+    else
+        printf("s2 is not empty\n");
+
+    destroy(s2);
+
+    return 0;
+}
