db2a301c9baf4501629883faa20272e13ca4045c
Adam Carter <adam.clayton.carter@gmail.com>
2021-01-27 21:17:03 -0500

    Complete Project 2 from Chapter 19

---
+++ ch19/projects/c19p02/rpn.c
@@ -0,0 +1,42 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "stackADT.h"
+
+int main(void)
+{
+    Stack s;
+    char ch;
+    int op1, op2;
+
+    s = create();
+
+    for (;;) {
+        printf("Enter an RPN expression: ");
+
+        do {
+            scanf(" %c", &ch);
+            if ('0' <= ch && ch <= '9')
+                push(s, ch - '0');
+            else if (ch == '+' || ch == '-' ||
+                     ch == '*' || ch == '/') {
+                op2 = pop(s);
+                op1 = pop(s);
+                switch (ch) {
+                    case '+': push(s, op1 + op2); break;
+                    case '-': push(s, op1 - op2); break;
+                    case '*': push(s, op1 * op2); break;
+                    case '/': push(s, op1 / op2); break;
+                }
+            } else if (ch != '=')
+                exit(EXIT_SUCCESS);
+        } while (ch != '=');
+
+        printf("Value of expression: %d\n", pop(s));
+        make_empty(s);
+    }
+
+    destroy(s);
+
+    return 0;
+}
