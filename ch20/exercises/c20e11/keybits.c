ecde4e91ce993f2654fbf4143728506e9c7c706e
Adam Carter <adam.clayton.carter@gmail.com>
2021-02-04 17:48:43 -0500

    Solve Exercise 11 from Chapter 20

---
+++ ch20/exercises/c20e11/keybits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define SHIFT_BIT 1
+#define CTRL_BIT  2
+#define ALT_BIT   4
+
+int main(void)
+{
+    int key_code = 8;
+    
+#ifdef WRONG
+    /* The equivalence operator has higher precedence than the bit-wise And operator. */
+    if (key_code & (SHIFT_BIT | CTRL_BIT | ALT_BIT) == 0)
+        printf("No modifier keys pressed\n");
+#endif
+
+#ifdef CORRECT
+    if ((key_code & (SHIFT_BIT | CTRL_BIT | ALT_BIT)) == 0)
+        printf("No modifier keys pressed\n");
+#endif
+
+    return 0;
+}
