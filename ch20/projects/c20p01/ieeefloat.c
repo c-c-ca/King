d4ac0d8040421650eea5e4024e73f9adfd182f1c
Adam Carter <adam.clayton.carter@gmail.com>
2021-02-06 16:13:10 -0500

    Modify Project 1 from Chapter 20

--- ch20/projects/c20p01/ieeefloat.c
+++ ch20/projects/c20p01/ieeefloat.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
+union IEEE_float {
+    struct {
+        unsigned int fraction: 23;
+        unsigned int exponent: 8;
+        unsigned int sign: 1;
+    } ieee_float;
+    float x;
+};
+
 int main(void)
 {
-    union {
-        struct {
-            unsigned int fraction: 23;
-            unsigned int exponent: 8;
-            unsigned int sign: 1;
-        } ieee_float;
-        float value;
-    } x;
+    union IEEE_float u;
 
-    x.ieee_float.fraction = 4788176;
-    x.ieee_float.exponent = 128;
-    x.ieee_float.sign = 0;
+    u.ieee_float.fraction = 4788176;
+    u.ieee_float.exponent = 128;
+    u.ieee_float.sign = 0;
 
-    printf("%f\n", x.value); /* prints "3.141590" */
+    printf("%f\n", u.x); /* prints "3.141590" */
 
     return 0;
 }
