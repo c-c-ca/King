e1c4b23813167e6753b9a62cdcaa2a481f94b1a1
Adam Carter <adam.clayton.carter@gmail.com>
2021-02-10 20:47:33 -0500

    Complete Project 1 from Chapter 22

---
+++ ch22/projects/c22p01/canopen.c
@@ -0,0 +1,31 @@
+/* Checks whether a file can be opened for reading */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *fp;
+    char **p;
+    bool cannot_open = false;
+
+    if (argc <= 1) {
+        printf("usage: canopen filename1 [filename2 ...]\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (p = argv; *++p != NULL;)
+        if ((fp = fopen(*p, "r")) == NULL) {
+            printf("%s can't be opened\n", *p);
+            cannot_open = true;
+        } else {
+            printf("%s can be opened\n", *p);
+            fclose(fp);
+        }
+
+    if (cannot_open)
+        exit(EXIT_FAILURE);
+
+    return 0;
+}
