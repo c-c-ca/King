d51d75e04e3460e77831784a5171ad135ed119c1
Adam Carter <adam.clayton.carter@gmail.com>
2021-02-11 17:12:15 -0500

    Modify Project 2 from Chapter 22

--- ch22/projects/c22p02/toupper.c
+++ ch22/projects/c22p02/toupper.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     int ch;
 
     if (argc != 2) {
-        printf("usage: toupper file\n");
+        fprintf(stderr, "usage: toupper file\n");
         exit(EXIT_FAILURE);
     }
 
