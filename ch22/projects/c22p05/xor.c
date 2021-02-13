f48e266098aa4e0440f7d469b4af6d4146795fd6
Adam Carter <adam.clayton.carter@gmail.com>
2021-02-12 20:49:15 -0500

    Modify Project 5 from Chapter 22

--- ch22/projects/c22p05/xor.c
+++ ch22/projects/c22p05/xor.c
@@ -12,17 +12,17 @@ int main(int argc, char *argv[])
     int ch;
 
     if (argc != 3) {
-        printf("usage: xor input output\n");
+        fprintf(stderr, "usage: %s input output\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     if ((input_fp = fopen(argv[1], "rb")) == NULL) {
-        printf("Can't open %s\n", argv[1]);
+        fprintf(stderr, "Can't open %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
     if ((output_fp = fopen(argv[2], "wb")) == NULL) {
-        printf("Can't open %s\n", argv[2]);
+        fprintf(stderr, "Can't open %s\n", argv[2]);
         fclose(input_fp);
         exit(EXIT_FAILURE);
     }
