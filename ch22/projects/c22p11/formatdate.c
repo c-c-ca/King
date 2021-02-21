9b3ae552fb097a06b9f66dcae4d5f89f35d36ec0
Adam Carter <adam.clayton.carter@gmail.com>
2021-02-20 20:01:14 -0500

    Modify Project 11 from Chapter 22

--- ch22/projects/c22p11/formatdate.c
+++ ch22/projects/c22p11/formatdate.c
@@ -16,10 +16,14 @@ int main(int argc, char *argv[])
 	}
 
 	if (sscanf(argv[1], "%d/%d/%d", &month, &day, &year) == 3 ||
-	    sscanf(argv[1], "%d-%d-%d", &month, &day, &year) == 3) {
-		printf("%s %d, %d\n", month_names[month-1], day, year);
-		return 0;
-	}
+	    sscanf(argv[1], "%d-%d-%d", &month, &day, &year) == 3)
+		if (1 <= month && month <= 12) {
+			printf("%s %d, %d\n", month_names[month-1], day, year);
+			return 0;
+		} else {
+			fprintf(stderr, "Error: illegal date\n");
+			exit(EXIT_FAILURE);
+		}
 
 	fprintf(stderr, "Date not in the proper form\n");
 	exit(EXIT_FAILURE);
--- ch22/projects/c22p11/test_formatdate.sh
+++ ch22/projects/c22p11/test_formatdate.sh
@@ -1,5 +1,9 @@
 gcc -o formatdate formatdate.c
+./formatdate
 ./formatdate 7/4/1776
 ./formatdate 7-4-1776
 ./formatdate 7.4.1776
-./formatdate
+./formatdate 1/1/1776
+./formatdate 12/1/1776
+./formatdate 0/1/1776
+./formatdate 13/1/1776
