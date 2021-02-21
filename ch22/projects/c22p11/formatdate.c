6e771afe5ad60f96a5874b98d2a1394ae24de4af
Adam Carter <adam.clayton.carter@gmail.com>
2021-02-20 19:45:59 -0500

    Complete Project 11 from Chapter 22

---
+++ ch22/projects/c22p11/formatdate.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const char *month_names[] = {"January", "February", "March", 
+	                     "April", "May", "June",
+			     "July", "August", "September", 
+			     "October", "November", "December"};
+
+int main(int argc, char *argv[])
+{
+	int day, month, year;
+
+	if (argc != 2) {
+		fprintf(stderr, "usage %s date\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	if (sscanf(argv[1], "%d/%d/%d", &month, &day, &year) == 3 ||
+	    sscanf(argv[1], "%d-%d-%d", &month, &day, &year) == 3) {
+		printf("%s %d, %d\n", month_names[month-1], day, year);
+		return 0;
+	}
+
+	fprintf(stderr, "Date not in the proper form\n");
+	exit(EXIT_FAILURE);
+}
---
+++ ch22/projects/c22p11/test_formatdate.sh
@@ -0,0 +1,5 @@
+gcc -o formatdate formatdate.c
+./formatdate 7/4/1776
+./formatdate 7-4-1776
+./formatdate 7.4.1776
+./formatdate
