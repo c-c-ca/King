7b171e8df279f835785d79a9376d8ce484a05b1c
Adam Carter <adam.clayton.carter@gmail.com>
2021-02-22 20:45:39 -0500

    Complete Project 19 from Chapter 22

---
+++ ch22/projects/c22p19/unixtodos.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define CR     '\x0d'
+#define LF     '\x0a'
+#define CTRL_Z '\x1a'
+
+int main(int argc, char *argv[])
+{
+	FILE *in_fp, *out_fp;
+	int ch;
+
+	if (argc != 3) {
+		fprintf(stderr, "usage: %s infile outfile\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((in_fp = fopen(argv[1], "rb")) == NULL) {
+		fprintf(stderr, "Can't open %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((out_fp = fopen(argv[2], "wb")) == NULL) {
+		fprintf(stderr, "Can't open %s\n", argv[2]);
+		fclose(in_fp);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((ch = getc(in_fp)) != EOF)
+		if ((ch == LF && (putc(CR, out_fp) == EOF)) || putc(ch, out_fp) == EOF) {
+			fprintf(stderr, "Error during writing; %s terminated\n", argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	putc(CTRL_Z, out_fp);
+
+	fclose(in_fp);
+	fclose(out_fp);
+	return 0;
+}
