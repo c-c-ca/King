6446a8fa0059dbd8bc05c7d330c6ccae63c40d1a
Adam Carter <adam.clayton.carter@gmail.com>
2021-02-23 17:31:35 -0500

    Complete Project 1 from Chapter 23

---
+++ ch23/projects/c23p01/findroots.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	float a, b, c, discriminant;
+
+	printf("Enter value of a: ");
+	scanf("%f", &a); 
+
+	printf("Enter value of b: ");
+	scanf("%f", &b); 
+
+	printf("Enter value of c: ");
+	scanf("%f", &c); 
+
+	discriminant = b * b - 4.0 * a * c;
+
+	if (discriminant < 0.0) {
+		printf("Roots are complex\n");
+		return 0;
+	}
+
+	printf("Roots: %f, %f\n",
+		(-b + sqrt(discriminant)) / (2.0 * a),
+		(-b - sqrt(discriminant)) / (2.0 * a));
+	return 0;
+}
