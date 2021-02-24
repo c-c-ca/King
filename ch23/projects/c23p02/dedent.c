e66a762c49b180dab828cf0d26b01ff05f93e866
Adam Carter <adam.clayton.carter@gmail.com>
2021-02-24 15:37:45 -0500

    Complete Project 2 from Chapter 23

---
+++ ch23/projects/c23p02/dedent.c
@@ -0,0 +1,24 @@
+/* Removes white-space characters from the beginning of each line */
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(void)
+{
+	bool nonblank_seen = false;
+	int ch;
+
+	while ((ch = getchar()) != EOF) {
+		if (nonblank_seen)
+			putchar(ch);
+		else if (!isspace(ch)) {
+			nonblank_seen = true;
+			putchar(ch);
+		}
+		if (ch == '\n')
+			nonblank_seen = false;
+	}
+
+	return 0;
+}
