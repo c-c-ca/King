31e950333bfb5803d6cbb920dbc8e9f0a0d00542
Adam Carter <adam.clayton.carter@gmail.com>
2021-03-27 22:35:01 -0400

    Complete Project 4 from Chapter 27

---
+++ ch27/projects/c27p04/rootsofunity.c
@@ -0,0 +1,19 @@
+#include <complex.h>
+#include <stdio.h>
+#include <tgmath.h>
+
+int main(void)
+{
+	int k, n;
+	double complex root;
+
+	printf("Enter number of roots: ");
+	scanf("%d", &n);
+
+	for (k = 0; k < n; k++) {
+		root = exp(2 * M_PI * I * k / n);	
+		printf("%.2g + %.2gi\n", creal(root), cimag(root));
+	}
+
+	return 0;
+}
